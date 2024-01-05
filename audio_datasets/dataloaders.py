a36eb6f5f2e3ccc82ab22a9e3b41541fd5fbbe3d
Menoua Keshishian <mk4011@columbia.edu>
2024-01-04 22:34:54 -0500

    Add some error handling for dataloaders

--- audio_datasets/dataloaders.py
+++ audio_datasets/dataloaders.py
@@ -144,6 +144,11 @@ class LibriSpeechTokenDataloader(LibriSpeechDataloader):
     ):
         super().__init__(dataset_type=TokenizedDataset, **kwargs)
 
+        if "flat_labels" in kwargs:
+            raise ValueError(
+                "LibriSpeechTokenDataloader does not support `flat_labels`"
+            )
+
         self.data_config = {
             **self.data_config,
             "duration": duration,
@@ -164,6 +169,9 @@ class LibriSpeechMultiDataloader(LibriSpeechDataloader):
     ):
         super().__init__(dataset_type=MultiAnnotatedDataset, **kwargs)
 
+        if "target" in kwargs:
+            raise ValueError("LibriSpeechMultiDataloader does not support `target`")
+
         if labels is None:
             labels = {k: LABELS[k] for k in ("chars", "phones", "syllables", "words")}
 
@@ -172,187 +180,3 @@ class LibriSpeechMultiDataloader(LibriSpeechDataloader):
             "vocabulary": labels,
         }
         del self.data_config["target"]
-
-
-# def librispeech(
-#     target: str = "words",
-#     vocabulary: Optional[list[str]] = None,
-#     limits: Optional[Limits] = LIMITS_WORD["librispeech"]["max"],
-#     batch_size: int = 12,
-#     num_workers: int = 4,
-#     flat_labels: bool = False,
-#     audio_transform: Optional[Callable] = mel_spectrogram(),
-#     split: str = "dev",
-# ):
-#     sounds, annots = {}, {}
-#     sounds["train"], annots["train"] = LibriSpeech(subset="train-*")
-#     sounds["dev"], annots["dev"] = LibriSpeech(subset="dev-other")
-#     sounds["test"], annots["test"] = LibriSpeech(subset="test-clean")
-#     vocabulary = LABELS[target] if vocabulary is None else vocabulary
-#
-#     data_config = {
-#         "batch_first": True,
-#         "target": target,
-#         "vocabulary": vocabulary,
-#         "limits": limits,
-#         "audio_transform": audio_transform,
-#         "normalize": len([w for w in vocabulary if "|" in w]) > 0,
-#     }
-#
-#     mod_config = {
-#         "mod_speech": True,
-#         "mod_room": False,
-#         "mod_channel": True,
-#         "mod_scene": NonSpeech(),
-#         "mod_intensity": "mid",
-#     }
-#
-#     if split == "train":
-#         dataset = AnnotatedDataset(sounds["train"], annots["train"], **data_config)
-#         dataset.augment(**mod_config)
-#         return dataset.iterator(
-#             shuffle=True,
-#             flat_labels=flat_labels,
-#             batch_size=batch_size,
-#             num_workers=num_workers,
-#         )
-#     elif split == "dev":
-#         dataset = AnnotatedDataset(sounds["dev"], annots["dev"], **data_config)
-#         return dataset.iterator(
-#             shuffle=False,
-#             flat_labels=flat_labels,
-#             batch_size=max(batch_size // 2, 1),
-#             num_workers=num_workers,
-#         )
-#     elif split == "test":
-#         dataset = AnnotatedDataset(sounds["test"], annots["test"], **data_config)
-#         return dataset.iterator(
-#             shuffle=False,
-#             flat_labels=flat_labels,
-#             batch_size=max(batch_size // 2, 1),
-#             num_workers=num_workers,
-#         )
-#     else:
-#         raise ValueError()
-#
-#
-# def librispeech_sequence(
-#     target: str = "words",
-#     vocabulary: Optional[list[str]] = None,
-#     seq_size: int = 20,
-#     seq_min: int = 1,
-#     block_size: float = 8.0,
-#     batch_size: int = 12,
-#     seq_per_sample: float = 4.0,
-#     num_workers: int = 4,
-#     audio_transform: Optional[Callable] = mel_spectrogram(),
-#     split: str = "dev",
-# ):
-#     sounds, annots = {}, {}
-#     sounds["train"], annots["train"] = LibriSpeech(subset="train-*")
-#     sounds["dev"], annots["dev"] = LibriSpeech(subset="dev-other")
-#     sounds["test"], annots["test"] = LibriSpeech(subset="test-clean")
-#     vocabulary = LABELS[target] if vocabulary is None else vocabulary
-#
-#     data_config = {
-#         "batch_first": False,
-#         "target": target,
-#         "vocabulary": vocabulary,
-#         "seq_size": seq_size,
-#         "seq_min": seq_min,
-#         "block_size": block_size,
-#         "audio_transform": audio_transform,
-#         "normalize": len([w for w in vocabulary if "|" in w]) > 0,
-#     }
-#
-#     mod_config = {
-#         "mod_speech": False,
-#         "mod_room": False,
-#         "mod_channel": True,
-#         "mod_scene": NonSpeech(),
-#         "mod_intensity": "mid",
-#     }
-#
-#     batch_max = int(batch_size * seq_per_sample)
-#
-#     if split == "train":
-#         dataset = SequenceDataset(
-#             sounds["train"], annots["train"], return_clean=True, **data_config
-#         )
-#         dataset.augment(**mod_config)
-#         return dataset.iterator(
-#             shuffle=True,
-#             batch_size=batch_size,
-#             batch_max=batch_max,
-#             num_workers=num_workers,
-#         )
-#     elif split == "dev":
-#         dataset = SequenceDataset(sounds["dev"], annots["dev"], **data_config)
-#         return dataset.iterator(
-#             shuffle=False,
-#             batch_size=max(batch_size // 2, 1),
-#             batch_max=batch_max,
-#             num_workers=num_workers,
-#         )
-#     elif split == "test":
-#         dataset = SequenceDataset(sounds["test"], annots["test"], **data_config)
-#         return dataset.iterator(
-#             shuffle=False,
-#             batch_size=max(batch_size // 2, 1),
-#             batch_max=batch_max,
-#             num_workers=num_workers,
-#         )
-#     else:
-#         raise ValueError()
-#
-#
-# def librispeech_token(
-#     target: str = "words",
-#     vocabulary: Optional[list[str]] = None,
-#     limits: Limits = LIMITS_WORD["librispeech"]["max"],
-#     batch_size: int = 12,
-#     num_workers: int = 4,
-#     audio_transform: Optional[Callable] = mel_spectrogram(),
-#     split: str = "dev",
-# ):
-#     sounds, annots = {}, {}
-#     sounds["train"], annots["train"] = LibriSpeech(subset="train-*")
-#     sounds["dev"], annots["dev"] = LibriSpeech(subset="dev-other")
-#     sounds["test"], annots["test"] = LibriSpeech(subset="test-clean")
-#     vocabulary = LABELS[target] if vocabulary is None else vocabulary
-#
-#     data_config = {
-#         "target": target,
-#         "vocabulary": vocabulary,
-#         "batch_first": True,
-#         "limits": limits,
-#         "audio_transform": audio_transform,
-#         "normalize": len([w for w in vocabulary if "|" in w]) > 0,
-#     }
-#
-#     mod_config = {
-#         "mod_speech": True,
-#         "mod_room": False,
-#         "mod_channel": True,
-#         "mod_scene": NonSpeech(),
-#         "mod_intensity": "mid",
-#     }
-#
-#     if split == "train":
-#         dataset = TokenizedDataset(sounds["train"], annots["train"], **data_config)
-#         dataset.augment(**mod_config)
-#         return dataset.iterator(
-#             shuffle=True, batch_size=batch_size, num_workers=num_workers
-#         )
-#     elif split == "dev":
-#         dataset = TokenizedDataset(sounds["dev"], annots["dev"], **data_config)
-#         return dataset.iterator(
-#             shuffle=False, batch_size=max(batch_size // 2, 1), num_workers=num_workers
-#         )
-#     elif split == "test":
-#         dataset = TokenizedDataset(sounds["test"], annots["test"], **data_config)
-#         return dataset.iterator(
-#             shuffle=False, batch_size=max(batch_size // 2, 1), num_workers=num_workers
-#         )
-#     else:
-#         raise ValueError()
